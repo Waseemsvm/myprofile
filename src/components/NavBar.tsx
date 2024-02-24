fc7e9963a3f4e3709170ecfc3c12693f5ee3d9b1
Waseem Akram P <waseemsvm14@gmail.com>
2024-02-24 11:07:28 +0530

    menu fixed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,20 @@
 export default function NavBar() {
+  document.addEventListener(
+    "click",
+    function (event) {
+      if (!event.target?.closest(".menu")) {
+        // !event.target?.closest(".menu-button") ||
+        // if menu icon clicked then by pass
+        let menuIconClicked =
+          !!event.target?.closest(".menu-button") ||
+          !!event.target?.closest("#menu-toggle");
+        if (!menuIconClicked && document.querySelector("#menu-toggle"))
+          document.querySelector("#menu-toggle").checked = false;
+      }
+    },
+    { capture: true }
+  );
+
   return (
     <section className="top-nav">
       <h2>Waseem Akram P</h2>
